72b7ad4c73888c2d74dce974acbb8561b42fad8b
lewis nyoike <nyoikelewis@gmail.com>
2018-09-12 16:53:59 +0300

    Migrated the db

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180906171134) do
+ActiveRecord::Schema.define(version: 20180911224021) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -53,6 +53,10 @@ ActiveRecord::Schema.define(version: 20180906171134) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "project_category_id"
+    t.string "cover_photo_file_name"
+    t.string "cover_photo_content_type"
+    t.integer "cover_photo_file_size"
+    t.datetime "cover_photo_updated_at"
   end
 
   create_table "users", force: :cascade do |t|
