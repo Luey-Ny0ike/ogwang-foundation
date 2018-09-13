c35af2c2f1a252d8c9b8eb30d1cd0266e8a38eeb
lewis nyoike <nyoikelewis@gmail.com>
2018-09-13 17:14:42 +0300

    Included the large resize option

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,6 +1,6 @@
 class Project < ApplicationRecord
   belongs_to :project_category
 
-  has_attached_file :cover_photo, styles: { medium: '400x400>', thumb: '100x100>' }, default_url: '/images/:style/missing.png'
+  has_attached_file :cover_photo, styles: { large: '600x600>', medium: '400x400>', thumb: '100x100>' }, default_url: '/images/:style/missing.png'
   validates_attachment_content_type :cover_photo, content_type: /\Aimage\/.*\z/
 end
