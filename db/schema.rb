601b721274ff724e89ecac54e155bd8f4a641a74
lewis nyoike <nyoikelewis@gmail.com>
2018-09-25 02:18:29 +0300

    Migrated the db

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180911224021) do
+ActiveRecord::Schema.define(version: 20180924230938) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,6 +26,10 @@ ActiveRecord::Schema.define(version: 20180911224021) do
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "cover_photo_file_name"
+    t.string "cover_photo_content_type"
+    t.integer "cover_photo_file_size"
+    t.datetime "cover_photo_updated_at"
   end
 
   create_table "ckeditor_assets", force: :cascade do |t|
