b0aa6997dcdcfae54399c806bdc1567b945b6156
lewis nyoike <nyoikelewis@gmail.com>
2018-09-27 17:31:34 +0300

    Added the cover_photo image attachment

--- app/models/news.rb
+++ app/models/news.rb
@@ -1,2 +1,5 @@
 class News < ApplicationRecord
+
+  has_attached_file :cover_photo, styles: { background: '940x600>',large: '600x600>', medium: '400x400>', thumb: '100x100>' }, default_url: '/images/:style/missing.png'
+  validates_attachment_content_type :cover_photo, content_type: /\Aimage\/.*\z/
 end
