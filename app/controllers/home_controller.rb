9a193256642fd202fd36465f96c09f7e6e8fb403
lewis nyoike <nyoikelewis@gmail.com>
2018-10-14 21:55:02 +0300

    Loaded data to use in the views

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,8 @@
 class HomeController < ApplicationController
 
   def index
-    @projects = Project.all
+    @news = News.first(3)
+    @projects = Project.first(3)
+    @articles = Article.first(3)
   end
 end
