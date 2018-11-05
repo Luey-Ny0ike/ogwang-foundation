e3d08c7ed8724d313899b4c36b0efaabcb773411
lewis nyoike <nyoikelewis@gmail.com>
2018-11-05 17:48:02 +0300

    Returns only approved articles

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,6 +3,6 @@ class HomeController < ApplicationController
   def index
     @news = News.first(3)
     @projects = Project.first(3)
-    @articles = Article.first(3)
+    @articles = Article.where(approved: true).first(3)
   end
 end
