d9a6e478183d8d33bb87986cf8335381b1344835
lewis nyoike <nyoikelewis@gmail.com>
2018-11-05 16:12:35 +0300

    Setup for new user mailer

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -3,4 +3,9 @@ class UserMailer < ApplicationMailer
   @user = params[:user]
   mail(to: @user.email, subject: 'Thank You and Welcome to Ogwang foundation')
   end
+
+  def new_user_email
+    @user = params[:user]
+    mail(to: 'nyoikelewis@gmail.com', subject: 'New user has registered')
+  end
 end
