37effe2dc7e2c03314d380e5bb4b5c745dd44749
lewis nyoike <nyoikelewis@gmail.com>
2018-11-05 17:48:32 +0300

    Allowed new user params

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -17,7 +17,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
   end
 
   def account_update_params
-    params.require(:user).permit(:first_name, :last_name, :id_number, :institution, :email, :password, :password_confirmation, :gender, :current_password, :about)
+    params.require(:user).permit(:first_name, :last_name, :id_number, :institution, :email, :password, :approved, :password_confirmation, :gender, :current_password, :about)
   end
   def configure_sign_up_params
     devise_parameter_sanitizer.permit(:sign_up, keys: %i[first_name last_name id_number institution about gender])
@@ -25,6 +25,6 @@ class Users::RegistrationsController < Devise::RegistrationsController
 
   # If you have extra params to permit, append them to the sanitizer.
   def configure_account_update_params
-    devise_parameter_sanitizer.permit(:account_update, keys: %i[first_name last_name about gender])
+    devise_parameter_sanitizer.permit(:account_update, keys: %i[first_name last_name about gender approved])
   end
 end
