8fd4c0af9914ec2ee01278ea018c789724f3c5f8
lewis nyoike <nyoikelewis@gmail.com>
2018-11-05 18:31:00 +0300

    Acceptance email setup

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -8,4 +8,9 @@ class UserMailer < ApplicationMailer
     @user = params[:user]
     mail(to: 'nyoikelewis@gmail.com', subject: 'New user has registered')
   end
+  
+  def acceptance_email
+    @user = params[:user]
+    mail(to: @user.email, subject: 'Your application was successfull')
+  end
 end
