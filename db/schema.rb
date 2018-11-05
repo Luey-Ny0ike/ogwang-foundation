bb86a6519109adc764eedb028baf9f3c105449d7
lewis nyoike <nyoikelewis@gmail.com>
2018-11-05 15:35:38 +0300

    MIgrations to add the new params to the user tables

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181015153520) do
+ActiveRecord::Schema.define(version: 20181105080219) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -94,6 +94,9 @@ ActiveRecord::Schema.define(version: 20181015153520) do
     t.string "last_name"
     t.integer "id_number"
     t.string "institution"
+    t.text "about"
+    t.string "gender"
+    t.string "dob"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
