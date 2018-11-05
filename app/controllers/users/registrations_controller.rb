a00109e315c01da08c9d02fb4f3344175d5c3688
lewis nyoike <nyoikelewis@gmail.com>
2018-11-05 15:34:34 +0300

    Configuration for new user params

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -12,18 +12,18 @@ class Users::RegistrationsController < Devise::RegistrationsController
   private
 
   def sign_up_params
-    params.require(:user).permit(:first_name, :last_name, :id_number, :institution, :email, :password, :password_confirmation)
+    params.require(:user).permit(:first_name, :last_name, :id_number, :institution, :email, :password, :password_confirmation, :about, :gender)
   end
 
   def account_update_params
-    params.require(:user).permit(:first_name, :last_name, :id_number, :institution, :email, :password, :password_confirmation, :current_password)
+    params.require(:user).permit(:first_name, :last_name, :id_number, :institution, :email, :password, :password_confirmation, :gender, :current_password, :about)
   end
   def configure_sign_up_params
-    devise_parameter_sanitizer.permit(:sign_up, keys: %i[first_name last_name id_number institution])
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[first_name last_name id_number institution about gender])
   end
 
   # If you have extra params to permit, append them to the sanitizer.
   def configure_account_update_params
-    devise_parameter_sanitizer.permit(:account_update, keys: %i[first_name last_name])
+    devise_parameter_sanitizer.permit(:account_update, keys: %i[first_name last_name about gender])
   end
 end
