b10b3d2142ef8ba8570f6dd252c74891cb3e5574
lewis nyoike <nyoikelewis@gmail.com>
2019-08-30 14:14:20 +0300

    Fixed the issues

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -57,7 +57,6 @@ Rails.application.configure do
     enable_starttls_auto: true }
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
-
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
