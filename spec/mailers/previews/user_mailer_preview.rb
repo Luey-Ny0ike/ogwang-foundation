952fdde7957dca548b79fd584ac3c1f2e99c52ef
lewis nyoike <nyoikelewis@gmail.com>
2019-09-20 12:02:21 +0300

    For the user mailer previews

--- spec/mailers/previews/user_mailer_preview.rb
+++ spec/mailers/previews/user_mailer_preview.rb
@@ -1,4 +1,8 @@
 # Preview all emails at http://localhost:3000/rails/mailers/user_mailer
 class UserMailerPreview < ActionMailer::Preview
 
+  def new_user_email
+    @user = User.first
+    UserMailer.with(user: User.first).new_user_email
+  end
 end
