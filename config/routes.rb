f6421678d11fbc53dc7ab37e0790a78e6fbbcc31
lewis nyoike <nyoikelewis@gmail.com>
2019-09-20 10:55:16 +0300

    For the user profile routes

--- config/routes.rb
+++ config/routes.rb
@@ -10,4 +10,10 @@ Rails.application.routes.draw do
   resources :project_categories
   resources :projects
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  # User dashboard routes
+  get 'users', to: "users#index", as: "users"
+  get 'users/:id', to: "users#show", as: "user"
+  get 'users/:id/edit', to: "users#edit", as: "edit_user"
+  patch 'users/:id', to: "users#update"
 end
